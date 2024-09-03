a60252a9a4082878f56a12e1dae13347b751c5ba
Shivam Kapur <shivam@heady.io>
2024-09-03 20:22:19 +0530

    Enabled SPM

--- Package.swift
+++ Package.swift
@@ -5,6 +5,8 @@ import PackageDescription
 
 let package = Package(
     name: "Caishen",
+    defaultLocalization: "en",
+    platforms: [.iOS(.v12)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -15,7 +17,8 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Caishen"),
+            name: "Caishen",
+            resources: [.process("Resources")]),
         .testTarget(
             name: "CaishenTests",
             dependencies: ["Caishen"]),
