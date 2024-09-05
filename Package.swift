d3a36624c6f549a00692c96ad2b1a9ef1c042bbd
Shivam Kapur <shivam@heady.io>
2024-09-05 18:30:33 +0530

    Code cleanup

--- Package.swift
+++ Package.swift
@@ -16,8 +16,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "Caishen"),
+        .target(name: "Caishen"),
         .testTarget(
             name: "CaishenTests",
             dependencies: ["Caishen"]),
