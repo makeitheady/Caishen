0dbb61543c2479073593ae36b86e8021e22d32ef
Shivam Kapur <shivam@heady.io>
2024-09-05 17:50:50 +0530

    Path changes for podspec

--- Caishen/Package.swift
+++ Caishen/Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "Caishen",
+    defaultLocalization: "en",
     platforms: [.iOS(.v12)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
@@ -16,7 +17,8 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Caishen"),
+            name: "Caishen",
+            resources: [.process("Assets")]),
         .testTarget(
             name: "CaishenTests",
             dependencies: ["Caishen"]),
