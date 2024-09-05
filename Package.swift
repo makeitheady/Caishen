44491d7fc9ba0addbd51014ec97fc1b2c44c10b5
Shivam Kapur <shivam@heady.io>
2024-09-05 18:17:34 +0530

    Code cleanup

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Caishen",
+    defaultLocalization: "en",
+    platforms: [.iOS(.v12)],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "Caishen",
+            targets: ["Caishen"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(name: "Caishen"),
+        .testTarget(
+            name: "CaishenTests",
+            dependencies: ["Caishen"]),
+    ]
+)
